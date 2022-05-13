f12c968d1076a546d0a4e9a213d82206c91b5814
Jacob Mathew <jacobgmathew@gmail.com>
2022-05-13 15:46:07 -0700

    Minor bugfix.

--- packages/web/src/Pages/Plan/ParamsInput/ParamsInputInflation.tsx
+++ packages/web/src/Pages/Plan/ParamsInput/ParamsInputInflation.tsx
@@ -51,7 +51,6 @@ export const ParamsInputInflation = React.memo(
             onClick={() =>
               setParams(p => ({
                 ...p,
-                returns: getDefaultParams().returns,
                 inflation: getDefaultParams().inflation,
               }))
             }
