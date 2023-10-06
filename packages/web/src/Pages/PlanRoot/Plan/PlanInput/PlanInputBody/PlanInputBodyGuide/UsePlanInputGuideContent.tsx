b89ed8119ff44cf114f9b208f7632161cad9baba
Jacob Mathew <jacobgmathew@gmail.com>
2023-10-06 15:27:51 -0700

    Bugfix

--- packages/web/src/Pages/PlanRoot/Plan/PlanInput/PlanInputBody/PlanInputBodyGuide/UsePlanInputGuideContent.tsx
+++ packages/web/src/Pages/PlanRoot/Plan/PlanInput/PlanInputBody/PlanInputBodyGuide/UsePlanInputGuideContent.tsx
@@ -1,3 +1,4 @@
+import { historicalReturns } from '@tpaw/common'
 import { Contentful } from '../../../../../../Utils/Contentful'
 import { formatCurrency } from '../../../../../../Utils/FormatCurrency'
 import { formatPercentage } from '../../../../../../Utils/FormatPercentage'
@@ -13,7 +14,7 @@ export function usePlanInputGuideContent(type: PlanInputType) {
   const { getZonedTime } = useIANATimezoneName()
   const { nonPlanParams } = useNonPlanParams()
   const { planParams, currentMarketData } = useSimulation()
-  const { CAPE, bondRates, inflation } = currentMarketData
+  const {  inflation } = currentMarketData
 
   const formatDate = (epoch: number) =>
     new Date(epoch).toLocaleDateString('en-US', {
@@ -28,67 +29,14 @@ export function usePlanInputGuideContent(type: PlanInputType) {
   const content = contentForType.guide[planParams.advanced.strategy]
   const variables = {
     numRuns: `${nonPlanParams.numOfSimulationForMonteCarloSampling}`,
-    capeDate: formatDate(CAPE.closingTime),
-    capeEarningsDateStart: getZonedTime
-      .fromObject({
-        year: CAPE.averageAnnualRealEarningsForSP500For10Years.tenYearDuration
-          .start.year,
-        month:
-          CAPE.averageAnnualRealEarningsForSP500For10Years.tenYearDuration.start
-            .month,
-      })
-      .toFormat('MMMM yyyy'),
-    capeEarningsDateEnd: getZonedTime
-      .fromObject({
-        year: CAPE.averageAnnualRealEarningsForSP500For10Years.tenYearDuration
-          .end.year,
-        month:
-          CAPE.averageAnnualRealEarningsForSP500For10Years.tenYearDuration.end
-            .month,
-      })
-      .toFormat('MMMM yyyy'),
-    capeEarnings: formatCurrency(
-      CAPE.averageAnnualRealEarningsForSP500For10Years.value,
-      2,
-    ),
-    sp500: formatCurrency(CAPE.sp500, 2),
-    expectedReturnsStocksCAPE: CAPE.value.toFixed(2),
-    expectedReturnsStocksOneOverCAPE: formatPercentage(1)(CAPE.oneOverCAPE),
-    expectedReturnsStocksRegressionFull5Year: formatPercentage(1)(
-      CAPE.regression.full.fiveYear,
-    ),
-    expectedReturnsStocksRegressionFull10Year: formatPercentage(1)(
-      CAPE.regression.full.tenYear,
-    ),
-    expectedReturnsStocksRegressionFull20Year: formatPercentage(1)(
-      CAPE.regression.full.twentyYear,
-    ),
-    expectedReturnsStocksRegressionFull30Year: formatPercentage(1)(
-      CAPE.regression.full.thirtyYear,
-    ),
-    expectedReturnsStocksRegressionRestricted5Year: formatPercentage(1)(
-      CAPE.regression.restricted.fiveYear,
-    ),
-    expectedReturnsStocksRegressionRestricted10Year: formatPercentage(1)(
-      CAPE.regression.restricted.tenYear,
-    ),
-    expectedReturnsStocksRegressionRestricted20Year: formatPercentage(1)(
-      CAPE.regression.restricted.twentyYear,
-    ),
-    expectedReturnsStocksRegressionRestricted30Year: formatPercentage(1)(
-      CAPE.regression.restricted.thirtyYear,
-    ),
-    expectedReturnsRegresssionAverage: formatPercentage(1)(
-      CAPE.regressionAverage,
-    ),
-    expectedReturnsSuggested: formatPercentage(1)(CAPE.suggested),
-    bondsDate: formatDate(bondRates.closingTime),
-    expectedReturnsBonds5Year: formatPercentage(1)(bondRates.fiveYear),
-    expectedReturnsBonds10Year: formatPercentage(1)(bondRates.tenYear),
-    expectedReturnsBonds20Year: formatPercentage(1)(bondRates.twentyYear),
-    expectedReturnsBonds30Year: formatPercentage(1)(bondRates.thirtyYear),
     inflationDate: formatDate(inflation.closingTime),
     inflation: formatPercentage(1)(inflation.value),
+    historicalExpectedStockReturn: formatPercentage(1)(
+      historicalReturns.monthly.annualStats.stocks.ofBase.mean,
+    ),
+    historicalExpectedBondReturn: formatPercentage(1)(
+      historicalReturns.monthly.annualStats.bonds.ofBase.mean,
+    ),
   }
   return Contentful.replaceVariables(variables, content)
 }
